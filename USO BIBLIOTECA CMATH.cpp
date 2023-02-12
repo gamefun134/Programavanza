c76b55777b9af809849c459398a9d9647b78c30d
DANIEL GONZALEZ <118472353+gamefun134@users.noreply.github.com>
2023-02-11 23:09:19 -0500

    Add files via upload

---
+++ USO BIBLIOTECA CMATH.cpp
@@ -0,0 +1,20 @@
+/*
+AUTOR: DANIEL FERNANDO GONZALEZ CORTES
+FECHA: 8/02/2023
+TRABAJO: EJERCICIO DE LA BIBLIOTECA CMATH
+*/
+#include <iostream>
+using namespace std;
+#include <cmath>;
+int main() {
+
+  double numero;
+  //LE PEDIMOS UN NUMERO AL USUARIO
+  cout<<"\tDIGITE SU NUMERO :D"<<endl;
+  cin>>numero;
+
+  //IMPRIMIMOS LA RAIZ DEL NUMERO QUE LE PEDIMOS AL USUARIO
+  cout<<"LA RAIZ CUADRADA";
+    cout<<"La raiz cuadrada es: "<<endl;
+    cout<<">>\t"<<sqrt(numero);
+}
