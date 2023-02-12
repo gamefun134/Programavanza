9329292a905423ae117e8768e1db5ecaccc0ca60
DANIEL GONZALEZ <118472353+gamefun134@users.noreply.github.com>
2023-02-11 23:08:01 -0500

    Add files via upload

---
+++ TIPOS DE VARIABLES.cpp
@@ -0,0 +1,22 @@
+
+
+//DANIEL FERNANDO GONZALEZ CORTES
+//TIPOS DE VARIABLES EN C++
+
+#include <iostream>
+using namespace std;
+int main() {
+  cout<<"\nTIPOS FUNDAMENTALES EN C++"<<endl;
+
+  cout<<"**********************************"<<endl;
+  cout<<"\t char: \t"<<sizeof(char)<<endl;
+  cout<<"\t short: "<<sizeof(short)<<endl;
+  cout<<"\t int: \t"<<sizeof(int)<<endl;
+  cout<<"\t long: \t"<<sizeof(long)<<endl;
+  cout<<"\t float: "<<sizeof(float)<<endl;
+  cout<<"\t double: "<<sizeof(double)<<endl;
+  cout<<"\t long double: \t"<<sizeof(long double)<<endl;
+  cout<<"**********************************"<<endl;
+
+  return 0;
+}
