60a33caede2734f20381d4f2ceb8d8361da817f5
DANIEL GONZALEZ <118472353+gamefun134@users.noreply.github.com>
2023-04-12 08:54:21 -0500

    Add files via upload

---
+++ Cuartopunto.java
@@ -0,0 +1,34 @@
+public class Cuartopunto {
+    public static void main(String[] args) {
+      Figura f = new Figura(10, 10);
+      Rectangulo r = new Rectangulo(9, 5);
+      System.out.println("Area es: " + f.area());
+      System.out.println("Area es: " + r.area());
+    }
+  }
+  
+  class Figura {
+    double dim1;
+    double dim2;
+  
+    Figura(double a, double b) {
+      dim1 = a;
+      dim2 = b;
+    }
+  
+    Double area() {
+      System.out.println("Area dentro de la figura. ");
+      return (dim1 * dim2);
+    }
+  }
+  
+  class Rectangulo extends Figura {
+    Rectangulo(double a, double b) {
+      super(a, b);
+    }
+  
+    Double area() {
+      System.out.println("Area dentro del rectangulo. ");
+      return (dim1 * dim2);
+    }
+  }
